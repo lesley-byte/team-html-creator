076dcb08f81aeb8583f02d0169436c784d619bf1
lesley-byte <phosphorescence3@hotmail.com>
2022-12-14 10:17:58 -0500

    added comments in the js files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Starting off by requiring the necessary packages
+
 const employee = require('./lib/Employee');
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
@@ -14,7 +16,9 @@ console.log('Welcome to the Team Profile Generator!');
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+// created empty array to store employees
 const employees = [];
+// created questions to ask user about employees
 const questions = () => {
     return inquirer.prompt([
         {
@@ -34,29 +38,33 @@ const questions = () => {
         },
         {
             type: 'list',
-            message: "What is the employee's role?",
+            message: "What is the employee's role? (Use arrow keys and then press enter to select)",
             name: 'role',
             choices: ['Manager', 'Engineer', 'Intern'],
         },
         {
+            // This will only run if the user selects 'Manager' as the employee's role
             type: 'input',
             message: "What is the employee's office number?",
             name: 'officeNumber',
             when: (answers) => answers.role === 'Manager',
         },
         {
+            // This will only run if the user selects 'Engineer' as the employee's role
             type: 'input',
             message: "What is the employee's GitHub username?",
             name: 'github',
             when: (answers) => answers.role === 'Engineer',
         },
         {
+            // This will only run if the user selects 'Intern' as the employee's role
             type: 'input',
             message: "What school does the intern attend?",
             name: 'school',
             when: (answers) => answers.role === 'Intern',
         },
         {
+            // If the user selects 'Yes' to add another employee, the questions will run again
             type: 'confirm',
             message: 'Would you like to add another employee?',
             name: 'addEmployee',
@@ -64,6 +72,7 @@ const questions = () => {
     ])
     .then((answers) => {
         let employee;
+        // This creates a new Manager, Engineer, or Intern object based on the user's answers and selects the questions to ask the user.
         if (answers.role === 'Manager') {
             employee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
         } else if (answers.role === 'Engineer') {
@@ -71,6 +80,7 @@ const questions = () => {
         } else if (answers.role === 'Intern') {
             employee = new Intern(answers.name, answers.id, answers.email, answers.school);
         }
+        // This adds the employee to the employees array
         employees.push(employee);
         if (answers.addEmployee) {
             return questions();
@@ -82,10 +92,11 @@ const questions = () => {
 
 function writeFileAsync(path, data) {
     return new Promise((resolve, reject) => {
-        // check if output folder exists
+        // check if output folder exists and create it if it doesn't
         if (!fs.existsSync(OUTPUT_DIR)) {
             fs.mkdirSync(OUTPUT_DIR);
         }
+        // write file once the output folder exists
         fs.writeFile(path, data,function (err) {
             if (err) {
                 reject(err);
@@ -96,7 +107,7 @@ function writeFileAsync(path, data) {
     });
 }
 
-
+// call questions function to start the program
 questions()
     .then((employees) => {
         return render(employees);
@@ -110,7 +121,8 @@ questions()
     .catch((err) => console.error(err));
 
 
-
+// Directions from Kevin Ferguson:
+//--------------------------------------------------------------------------------------------
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!  
